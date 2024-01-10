7e68b71335ff516f31295b952c753b698feb3976
Hasnain Ahmad <hasnainahmadkhan04@gmail.com>
2024-01-10 16:07:38 +0500

    added OpenGraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,44 @@
 import Feed from "@components/Feed";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  openGraph: {
+    type: "website",
+    images: ["https://unsplash.com/photos/woman-with-dslr-camera-e616t35Vbeg"],
+    title: "Promptopia",
+    description: "Discover & Prompt AI Prompts",
+    url: "https://next-crud-hasnainahmad04.vercel.app/",
+  },
+  twitter: {
+    title: "Promptopia",
+    card: "summary_large_image",
+    images: ["https://unsplash.com/photos/woman-with-dslr-camera-e616t35Vbeg"],
+    site: "https://next-crud-hasnainahmad04.vercel.app/",
+    description: "Discover & Prompt AI Prompts",
+  },
+};
 
 const App = () => {
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
-      </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
-      </p>
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Discover & Share
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            AI-Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Promptopia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts
+        </p>
 
-      <Feed />
-    </section>
+        <Feed />
+      </section>
+    </>
   );
 };
 
